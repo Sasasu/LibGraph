f42a0bec7110377ec87117a14ea609122383b0e8
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-04-02 14:26:23 +0530

    Bellman Ford Added, Function headers Added

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<set>
+#include<cstdlib>
+#include<queue>
+#include<map>
 using namespace std;
 
-template <class T,class W>
+#define INF 10000000
+
+template <class T, class W>
 class Edge {
 	public:
 	T u, v;
@@ -17,14 +24,14 @@ class Edge {
 	}
 };
 
-template <class T,class W>
+template <class T, class W>
 class Graph {
-	set   < T >                       vertices;	// Vertex set
-	vector< Edge<T,W> >                 edges;	// Edges set
+	set   < T >                     vertices;       // Vertex set
+	vector< Edge<T,W> >             edges;	        // Edges set
 	map< T, vector< pair< T, W> > > AdjList;	// Adjacency List
-	map< T, map< T, W> >            AdjMat;	// Adjacency Matrix
-	map< T, int>                    Level;  //BFS output levels
-	map< T, bool>                   bfsVisit; //BFS Visit 
+	map< T, map< T, W> >            AdjMat;	        // Adjacency Matrix
+	map< T, int>                    Level;          // BFS output levels
+	map< T, bool>                   bfsVisit;       // BFS Visit 
  	public:
 	Graph(){}
 	Graph(vector<T>, vector< Edge<T,W> >);
@@ -33,19 +40,37 @@ class Graph {
 	void printAdjacencyList();
 	void createAdjacencyMatrix();
 	void printAdjacencyMatrix();
-	map<T, W> shortestPathFrom(T);
+	map<T, W> dijkstras(T);
+	map<T, W> bellmanFord(T);
+	map<T, W> shortestPathFrom(T, bool negative = false);
 	void printLevel(T);
 	void bfs(T);	
 	bool hasCycleUtil(T, map<T, bool>&, map<T, bool>&);
 	bool hasCycle();
 };
 
+/*
+ * Graph class Constructor
+ * @param (vector<T>) v
+ *   vector of vertices
+ * @param (vector<EdgeT,W>>) e
+ *   vector of edges
+ */
 template <class T,class W>
 Graph<T,W>::Graph(vector<T> v, vector< Edge<T,W> > e){
 	vertices = v;
 	edges    = e;
 }
 
+/*
+ * Function to add edge
+ * @param (T) u
+ *   vertex
+ * @param (T) v
+ *   vertex
+ * @param (W) w 
+ *   weight of edge from u to v
+ */
 template <class T,class W>
 void Graph<T,W>::addEdge(T u, T v, W w){
 	edges.push_back(Edge<T,W>(u, v, w));
@@ -53,6 +78,9 @@ void Graph<T,W>::addEdge(T u, T v, W w){
 	vertices.insert(v);
 }
 
+/* 
+ * Function to create Adjacency list
+ */
 template <class T,class W>
 void Graph<T,W>::createAdjacencyList(){
 	for(int i=0;i<edges.size();i++){
@@ -61,6 +89,9 @@ void Graph<T,W>::createAdjacencyList(){
 	}
 }
 
+/*
+ * Function to print Adjacency list
+ */
 template <class T,class W>
 void Graph<T,W>::printAdjacencyList(){
 	typename map< T, vector< pair< T, W> > >::iterator u;
@@ -74,6 +105,9 @@ void Graph<T,W>::printAdjacencyList(){
 	}
 }
 
+/*
+ * Function to create Adjacency Matrix
+ */
 template <class T,class W>
 void Graph<T,W>::createAdjacencyMatrix(){
 	for(int i=0;i<edges.size();i++){
@@ -82,6 +116,9 @@ void Graph<T,W>::createAdjacencyMatrix(){
 	}
 }
 
+/*
+ * Fucnction to print Adjacency Matrix
+ */
 template <class T,class W>
 void Graph<T,W>::printAdjacencyMatrix(){
 	typename map< T, map< T, int > >::iterator i;
@@ -93,14 +130,23 @@ void Graph<T,W>::printAdjacencyMatrix(){
 	}
 }
 
-template <class T,class W>
-map<T, W> Graph<T,W>::shortestPathFrom(T s){
+/*
+ * Function to calculate Single Source Shortest Paths using Dijkstra's Algorithm
+ * For graphs with positive edge weights only
+ * For graphs consiting negative edge weights, use bellmanFord(T)
+ * @param (T) s
+ *   source vertex
+ * @returns (map<T,W>) 
+ *   mapping of weights of shortest paths
+ */
+template <class T, class W>
+map<T, W> Graph<T, W>::dijkstras(T s){
 	if(AdjList.size()==0)
 		createAdjacencyList();
 	map<T, W> dist;
 	typename set<T>::iterator it;
 	for(it=vertices.begin(); it!=vertices.end(); it++)
-		dist[*it] = 1000000;
+		dist[*it] = INF;
 	priority_queue< pair<W, T> > q;
 	q.push(make_pair(0, s));
 	dist[s] = 0;
@@ -120,6 +166,58 @@ map<T, W> Graph<T,W>::shortestPathFrom(T s){
 	}
 	return dist;
 }
+
+/*
+ * Function to calculate Single Source Shortest Paths using Bellman Ford Cycle
+ * For graphs consisting negative edge weights
+ * @param (T) s
+ *   source vertex
+ * @returns (map<T,W>) 
+ *   mapping of weights of shortest paths
+ */
+template <class T,  class W>
+map<T, W> Graph<T, W>::bellmanFord(T s){
+	map<T, W> dist;
+	typename set<T>::iterator it;
+	for(it=vertices.begin(); it!=vertices.end(); it++)
+		dist[*it] = INF;
+	dist[s] = 0;
+	typename vector< Edge<T, W> >::iterator e;
+	for(int i=0; i<vertices.size()-1;i++){
+		for(e=edges.begin(); e!=edges.end(); e++){
+			T u = e->u,
+			  v = e->v;
+			W w = e->w;
+			if(dist[u]!=INF && dist[u]+w < dist[v])
+				dist[v] = dist[u] + w;
+		}
+	}
+	return dist;
+}
+
+/*
+ * Function to calculate Single Source Shortest Paths
+ * @param (T) s
+ *   source vertex
+ * @param (bool) negative
+ *   true if graph consists negative edge weights, default value false
+ * @returns (map<T,W>) 
+ *   mapping of weights of shortest paths
+ */
+template <class T, class W>
+map<T, W> Graph<T,W>::shortestPathFrom(T s, bool negative){
+	if(negative)
+		return bellmanFord(s);
+	return dijkstras(s);
+}
+
+/* 
+ * Function to apply Breadth First Search on graph
+ * @param (T) source
+ *   source of required BFS
+ * @returns (map<T, int>) 
+ *   mapping of vertices to their levels in BFS tree
+ */
 template <class T,class W>
 void Graph<T,W>::bfs(T source)
 {
@@ -157,6 +255,18 @@ void Graph<T,W>::printLevel(T source)
 	}
 }
 
+/* 
+ * Utility function for detecting cycle in graph
+ * Used by hasCycle(T, map<T, bool>&, map<T, bool>&)
+ * @param (T) v
+ *   current vertex
+ * @param (vector<T, bool>) visited
+ *   vector of visit status of vertex
+ * @param (vector T, bool>) racStack
+ *   vector of stack status of vertex
+ * @returns (bool) 
+ *   true if graph has cycle, else false
+ */
 template <class T, class W>
 bool Graph<T, W>::hasCycleUtil(T v, map<T, bool> &visited, map<T, bool>  &recStack){
 	if(visited[v] == false) {
@@ -177,6 +287,11 @@ bool Graph<T, W>::hasCycleUtil(T v, map<T, bool> &visited, map<T, bool>  &recSta
 	return false;
 }
 
+/*
+ * Function to detect cycle in graph
+ * @returns (bool)
+ *   true if graph has cycle, else false
+ */
 template <class T, class W>
 bool Graph<T, W>::hasCycle(){
 	map<T, bool> visited, recStack;
